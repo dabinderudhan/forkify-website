468a3132fbafc6dd2b4a49c5fa7bccda3e446caa
dabinderudhan <dabinder.udhan@gmail.com>
2022-02-04 11:05:45 +0530

    clearBookmarks activated

--- src/js/model.js
+++ src/js/model.js
@@ -142,7 +142,7 @@ init();
 const clearBookmarks = function () {
   localStorage.clear('bookmarks');
 };
-// clearBookmarks();
+clearBookmarks();
 
 export const uploadRecipe = async function (newRecipe) {
   try {
